8722c50e88bfbb3cbb27fc9bfeba9d823e885f08
yahav_ben_simon <152993344+Abcd20000@users.noreply.github.com>
2026-06-21 17:35:18 +0300

    Add files via upload
    It probably does not work I didn't check it and I need to fix it. it is the preprose part,I also fix the start but I think it is anyway not needed I just forget I did it.

--- start.c
+++ start.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
 #include <stdio.h>
+#include <math.h>
 //const int sttatic arr_optcode[]
-const int OPCODE_MOVE_RIGHT=26;
-const int RS_MOVE_RIGHT = 20;
-const int RT_MOVE_RIGHT = 16;
-const int RD_MOVE_RIGHT = 11;
-const int SHAMT_MOVE_RIGHT = 6;
-const int FUNCT_MOVE_RIGHT = 0;
-const int I_MOVE_RIGHT = 0;
-const int SIZE_SMALL_FIELD=5;
-const int SIZE_MEDIUM_FILED = 6;
-const int SIZE_I_IMMEDIATE = 16;
+//R type locitions
+const int   OPCODE_MOVE_RIGHT=26;
+const int   RS_MOVE_RIGHT = 20;
+const int   RT_MOVE_RIGHT = 16;
+const int   RD_MOVE_RIGHT = 11;
+const int   SHAMT_MOVE_RIGHT = 6;
+const int   FUNCT_MOVE_RIGHT = 0;
+//I type locitions
+const int   I_MOVE_RIGHT = 0;
+//J type locitions
+const int   ADDRESS_MOVE_RIGHT = 26;
+//type sizes the amount of ones is the size
+const int   OPCODE_SIZE = 2<<6-1;//6
+const int   RS_SIZE = 2<<5-1;//5
+const int   RT_SIZE = 2<<5-1;//5
+const int   RD_SIZE = 2<<5-1;//5
+const int   SHAMT_SIZE = 2<<5-1;//5
+const int   FUNCT_SIZE = 2<<6-1;//6
+const int   IMMEDIATE_SIZE = 2<<15-1;//15
+const int   ADDRESS_SIZE = 2<<26-1;//26
+//OPT_CODES
+const int R_OPTCODE = 0;
+const int J_OPTCODE = 5;//need to check
 int main()
 {   
     int IC=100;
@@ -22,23 +36,34 @@ int main()
 void commend_parse(int commend)
 {
     int opcode = commend << OPCODE_MOVE_RIGHT;
-    if (opcode ==0)//if opcode is 0 then it is type R
+    if (opcode ==R_OPTCODE)//if opcode is 0 then it is type R
     {
-
+        R_parse(commend);
     }
+    else
+    {
+        if(opcode == J_OPTCODE)
+        {
+            J_parse(commend);
+        }
+    }
+    I_parse(commend);
 }
 void I_parse(int commend)
 {
-    int opcode = (commend << OPCODE_MOVE_RIGHT) &&SIZE_MEDIUM_FILED;
-    int rs = (commend << RS_MOVE_RIGHT) && SIZE_SMALL_FIELD;
-    int rt = (commend << RT_MOVE_RIGHT) && SIZE_SMALL_FIELD;
-    int immediate = (commend<< I_MOVE_RIGHT)&&SIZE_I_IMMEDIATE;
+    int opcode = (commend << OPCODE_MOVE_RIGHT) && OPCODE_SIZE;
+    int rs = (commend << RS_MOVE_RIGHT) && RS_SIZE;
+    int rt = (commend << RT_MOVE_RIGHT) && RT_SIZE;
+    int immediate = (commend<< I_MOVE_RIGHT)&&IMMEDIATE_SIZE;
 }
-void R_parse()
+void R_parse(int commend)
 {
-
+    int opcode = (commend << OPCODE_MOVE_RIGHT) && OPCODE_SIZE;
+    int rs = (commend << RS_MOVE_RIGHT) && RS_SIZE;
+    int rt = (commend << RT_MOVE_RIGHT) && RT_SIZE;
+    
 }
-void J_parse()
+void J_parse(int commend)
 {
 
 }
